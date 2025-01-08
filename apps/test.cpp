ef1dbb4c000d159c3e4314a9fe70f6f06eba7fa2
wangmingyu <wangmingyu21a@mails.ucas.ac.cn>
2025-01-08 17:15:22 +0800

    feat: yaml-cpp, glog, gflags - 引入外部库: yaml-cpp, glog, gflags

--- apps/test.cpp
+++ apps/test.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <string>
 
-int main() {
+#include <yaml-cpp/yaml.h>
+#include <yaml-cpp/node/node.h>
+#include <yaml-cpp/node/parse.h>
+
+#include <glog/logging.h>
+
+int main(int argc, char** argv) {
+    google::InitGoogleLogging(argv[0]);
     std::cout << "hello world" << std::endl;
+    YAML::Node config = YAML::LoadFile("../config/test.yaml");
+    // const std::string name = config["name"].as<std::string>();
+    const int age = config["age"].as<int>();
+    // std::cout << name << std::endl;
+    std::cout << age << std::endl;
     return 0;
 }
