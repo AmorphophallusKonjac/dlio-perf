477a59e7e48b851bcd7d85be6f08f2811f97a5ea
wangmingyu <wangmingyu21a@mails.ucas.ac.cn>
2025-03-22 04:08:08 +0800

    add: rank test

--- scripts/start_test.py
+++ scripts/start_test.py
@@ -6,7 +6,6 @@ import yaml
 from tqdm import tqdm
 from concurrent.futures import ThreadPoolExecutor
 
-hosts = ["10.118.0.161"]
 ssh_user = "root"
 sar_cmd = "sar -o /var/log/sar_%HOST%_%TIMESTAMP%.log 1"
 
@@ -16,7 +15,7 @@ def start_remote_sar(host):
     timestamp = subprocess.check_output(["date", "+%s"]).decode().strip()
     cmd = sar_cmd.replace("%HOST%", host).replace("%TIMESTAMP%", timestamp)
     ssh_args = [
-        "ssh", 
+        "ssh", "-o", "StrictHostKeyChecking=no", 
         f"{ssh_user}@{host}",
         f"nohup {cmd} >/dev/null 2>&1 & echo $! > /tmp/sar_{timestamp}.pid"
     ]
@@ -33,42 +32,35 @@ def stop_remote_sar(host):
         "kill -9 $pid && rm /tmp/sar_*.pid || echo 'not running'"
     )
     subprocess.Popen(
-        ["ssh", f"{ssh_user}@{host}", kill_cmd],
+        ["ssh", "-o", "StrictHostKeyChecking=no", f"{ssh_user}@{host}", kill_cmd],
         stdout=subprocess.DEVNULL,
         stderr=subprocess.DEVNULL
     )
 
 def copy_sar_log(host, folder):
-    scp_cmd = f"scp {ssh_user}@{host}:/var/log/sar* {folder}"
+    scp_cmd = f"scp -o StrictHostKeyChecking=no {ssh_user}@{host}:/var/log/sar* {folder}"
     subprocess.run(scp_cmd, shell=True)
     rm_cmd = "rm -f /var/log/sar*"
     subprocess.Popen(
-        ["ssh", f"{ssh_user}@{host}", rm_cmd],
+        ["ssh", "-o", "StrictHostKeyChecking=no", f"{ssh_user}@{host}", rm_cmd],
         stdout=subprocess.DEVNULL,
         stderr=subprocess.DEVNULL
     )
 
 def start_sar(hosts):
-    timestamp = subprocess.check_output(["date", "+%s"]).decode().strip()
-    cmd = sar_cmd.replace("%HOST%", "client").replace("%TIMESTAMP%", timestamp)
-    cmd = f"nohup {cmd} >/dev/null 2>&1 & echo $! > /tmp/sar_{timestamp}.pid"
-    subprocess.run(cmd, shell=True)
     with ThreadPoolExecutor(max_workers=3) as executor:
         processes = executor.map(start_remote_sar, hosts)
 
 def stop_sar(hosts, folder):
-    cmd = (
-        "pid=$(cat /tmp/sar_*.pid);"
-        "kill -9 $pid && rm /tmp/sar_*.pid || echo 'not running'"
-    )
-    subprocess.run(cmd, shell=True)
-    cp_cmd = f"cp /var/log/sar* {folder}"
-    subprocess.run(cp_cmd, shell=True)
-    rm_cmd = f"rm /var/log/sar*"
-    subprocess.run(rm_cmd, shell=True)
     for host in hosts:
         stop_remote_sar(host)
+    for host in hosts:
         copy_sar_log(host, folder)
+
+def drop_caches(hosts):
+    for host in hosts:
+        subprocess.run(["ssh", "-o", "StrictHostKeyChecking=no", f"{ssh_user}@{host}", "sync"])
+        subprocess.run(["ssh", "-o", "StrictHostKeyChecking=no", f"{ssh_user}@{host}", "echo 3 > /proc/sys/vm/drop_caches"])
 
 def deal_with_yaml(args):
     file_path = args.config
@@ -78,9 +70,15 @@ def deal_with_yaml(args):
     with open(file_path, 'r', encoding='utf-8') as f:
         dlio_config = yaml.safe_load(f)
     servers = test_config["servers"]
+    clients = test_config["clients"]
+    hosts = servers + clients
     output_base = test_config["output_folder"]
     var_path = test_config["var"]["name"]
     rank = args.rank
+    total_rank = rank * len(clients)
+    with open("hosts.txt", 'w') as f:
+        for client in clients:
+            f.write(f"{client} slots={rank}\n")
     for i in tqdm(range(test_config["var"]["start_val"], test_config["var"]["end_val"], test_config["var"]["step"])):
         # prepare output_folder
         sub_folder = "_".join(var_path)
@@ -102,24 +100,69 @@ def deal_with_yaml(args):
                 indent=2
             )
         # drop cache
-        subprocess.run(["sync"])
-        subprocess.run("echo 3 > /proc/sys/vm/drop_caches", shell=True)
-        for host in servers:
-            subprocess.run(["ssh", f"{ssh_user}@{host}", "sync"])
-            subprocess.run(["ssh", f"{ssh_user}@{host}", "echo 3 > /proc/sys/vm/drop_caches"])
+        drop_caches(hosts)
         # start sar
-        start_sar(servers)
-        time.sleep(3)
+        start_sar(hosts)
+        time.sleep(60)
         # start benchmark
-        mpi_cmd=f"mpirun --allow-run-as-root -n {rank} /root/dlio-perf/bin/dlio_perf --config run.yaml"
+        mpi_cmd=f"mpirun --allow-run-as-root -np {total_rank} --hostfile hosts.txt /root/dlio-perf/bin/dlio_perf --config run.yaml"
         subprocess.run(mpi_cmd, shell=True)
         # stop sar
-        stop_sar(servers, output_folder)
+        stop_sar(hosts, output_folder)
     rm_cmd = "rm run.yaml"
+    subprocess.run(rm_cmd, shell=True)
+    rm_cmd = "rm hosts.txt"
     subprocess.run(rm_cmd, shell=True)
 
 def deal_with_rank(args):
-    pass
+    file_path = args.config
+    with open(file_path, 'r', encoding='utf-8') as f:
+        test_config = yaml.safe_load(f)
+    file_path = test_config["base_config"]
+    with open(file_path, 'r', encoding='utf-8') as f:
+        dlio_config = yaml.safe_load(f)
+    servers = test_config["servers"]
+    clients = test_config["clients"]
+    hosts = servers + clients
+    output_base = test_config["output_folder"]
+    for i in tqdm(range(test_config["var"]["start_val"], test_config["var"]["end_val"], test_config["var"]["step"])):
+        # calculate rank
+        rank = i
+        total_rank = rank * len(clients)
+        # prepare hosts.txt
+        with open("hosts.txt", 'w') as f:
+            for client in clients:
+                f.write(f"{client} slots={rank}\n")
+        # prepare output_folder
+        sub_folder = f"client_{len(clients)}_rank"
+        sub_folder = sub_folder + f"_{i}" 
+        output_folder = os.path.join(output_base, sub_folder)
+        os.makedirs(output_folder, exist_ok=True)
+        # prepare run_config
+        run_config = dlio_config
+        run_config["output"]["folder"] = output_folder
+        with open("run.yaml", 'w', encoding='utf-8') as f:
+            yaml.safe_dump(
+                run_config, f,
+                allow_unicode=True,
+                sort_keys=False,
+                indent=2
+            )
+        # drop cache
+        drop_caches(hosts)
+        # start sar
+        start_sar(hosts)
+        time.sleep(60)
+        # start benchmark
+        mpi_cmd=f"mpirun --allow-run-as-root -np {total_rank} --hostfile hosts.txt /root/dlio-perf/bin/dlio_perf --config run.yaml"
+        subprocess.run(mpi_cmd, shell=True)
+        # stop sar
+        stop_sar(hosts, output_folder)
+    rm_cmd = "rm run.yaml"
+    subprocess.run(rm_cmd, shell=True)
+    rm_cmd = "rm hosts.txt"
+    subprocess.run(rm_cmd, shell=True)
+
         
 
 if __name__ == "__main__":
